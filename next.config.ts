3109bd2c07f64f2ff5a8b24ebcf46f2b1b17c119
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-02-01 08:37:04 +0900

    배포준비

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   async rewrites() {
-    const apiUrl = process.env.SERVER_API_URL || "http://localhost:8089";
+    const apiUrl = process.env.SERVER_API_URL || "http://localhost:8083";
     console.log(`[Next.js] Proxying /api requests to: ${apiUrl}`);
 
     return [
